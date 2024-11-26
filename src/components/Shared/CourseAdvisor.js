bc4101e1b5326159b131d8e18657f7909db758a9
thealamindev <ll.alamin.ll@gmail.com>
2024-11-26 16:33:35 +0600

    first commit

---
+++ src/components/Shared/CourseAdvisor.js
@@ -0,0 +1,428 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+const CourseAdvisor = () => {
+	return (
+		<>
+			<div className="advisor-area bg-f9f9f9 ptb-100">
+				<div className="container">
+					<div className="section-title">
+						<span className="sub-title">Instructor</span>
+						<h2>Course Advisor</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua.
+						</p>
+					</div>
+
+					<div className="row justify-content-center">
+						<div className="col-lg-4 col-md-6">
+							<div className="single-advisor-box">
+								<div className="advisor-image">
+									<Image
+										src="/images/advisor/advisor1.jpeg"
+										width={590}
+										height={450}
+										alt="image"
+									/>
+								</div>
+
+								<div className="advisor-content">
+									<h3>Salim Sadman Siam</h3>
+									<span className="sub-title">
+									FOUNDER & CEO 
+									</span>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur
+										adipiscing elit, sed do eiusmod tempor
+										incididunt ut labore et dol aliqua.
+									</p>
+
+									<ul className="social-link">
+										<li>
+											<a
+												href="https://www.facebook.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-facebook"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.twitter.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.instagram.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-instagram"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.linkedin.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-linkedin"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-lg-4 col-md-6">
+							<div className="single-advisor-box">
+								<div className="advisor-image">
+									<Image
+										src="/images/advisor/advisor2.jpeg"
+										width={590}
+										height={450}
+										alt="image"
+									/>
+								</div>
+
+								<div className="advisor-content">
+									<h3>Mashrafe Rahim</h3>
+									<span className="sub-title">
+										HOD Of ICT
+									</span>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur
+										adipiscing elit, sed do eiusmod tempor
+										incididunt ut labore et dol aliqua.
+									</p>
+									<ul className="social-link">
+										<li>
+											<a
+												href="https://www.facebook.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-facebook"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.twitter.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.instagram.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-instagram"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.linkedin.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-linkedin"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-lg-4 col-md-6">
+							<div className="single-advisor-box">
+								<div className="advisor-image">
+									<img
+										src="/images/advisor/advisor3.jpeg"
+										width={590}
+										height={450}
+										alt="image"
+									/>
+								</div>
+
+								<div className="advisor-content">
+									<h3>Saif Al Islam</h3>
+									<span className="sub-title">
+										HOD Of Math
+									</span>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur
+										adipiscing elit, sed do eiusmod tempor
+										incididunt ut labore et dol aliqua.
+									</p>
+									<ul className="social-link">
+										<li>
+											<a
+												href="https://www.facebook.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-facebook"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.twitter.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.instagram.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-instagram"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.linkedin.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-linkedin"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div className="col-lg-4 col-md-6">
+							<div className="single-advisor-box">
+								<div className="advisor-image">
+									<img
+										src="/images/advisor/advisor4.jpeg"
+										width={590}
+										height={450}
+										alt="image"
+									/>
+								</div>
+
+								<div className="advisor-content">
+									<h3>Faiaz Hasan</h3>
+									<span className="sub-title">
+										HOD Of Physics
+									</span>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur
+										adipiscing elit, sed do eiusmod tempor
+										incididunt ut labore et dol aliqua.
+									</p>
+									<ul className="social-link">
+										<li>
+											<a
+												href="https://www.facebook.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-facebook"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.twitter.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.instagram.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-instagram"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.linkedin.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-linkedin"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div className="col-lg-4 col-md-6">
+							<div className="single-advisor-box">
+								<div className="advisor-image">
+									<img
+										src="/images/advisor/advisor5.jpeg"
+										width={590}
+										height={450}
+										alt="image"
+									/>
+								</div>
+
+								<div className="advisor-content">
+									<h3>Sonod Sadman</h3>
+									<span className="sub-title">
+										ICT Mentor
+									</span>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur
+										adipiscing elit, sed do eiusmod tempor
+										incididunt ut labore et dol aliqua.
+									</p>
+									<ul className="social-link">
+										<li>
+											<a
+												href="https://www.facebook.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-facebook"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.twitter.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.instagram.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-instagram"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.linkedin.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-linkedin"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div className="col-lg-4 col-md-6">
+							<div className="single-advisor-box">
+								<div className="advisor-image">
+									<img
+										src="/images/advisor/advisor3.jpg"
+										width={590}
+										height={450}
+										alt="image"
+									/>
+								</div>
+
+								<div className="advisor-content">
+									<h3>Sahajalal Hossen Safi</h3>
+									<span className="sub-title">
+									Physics Mentor
+									</span>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur
+										adipiscing elit, sed do eiusmod tempor
+										incididunt ut labore et dol aliqua.
+									</p>
+									<ul className="social-link">
+										<li>
+											<a
+												href="https://www.facebook.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-facebook"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.twitter.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.instagram.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-instagram"></i>
+											</a>
+										</li>
+										<li>
+											<a
+												href="https://www.linkedin.com/"
+												className="d-block"
+												target="_blank"
+												rel="noreferrer"
+											>
+												<i className="bx bxl-linkedin"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default CourseAdvisor;
